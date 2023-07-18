33d5cf9100f6afde2fc808d36d8d83689324a929
Lucas <a@a.fr>
2023-07-18 14:36:06 +0200

    Root navbar

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,4 +11,46 @@ class IndexController extends AbstractController
   {
     return $this->twig->render('index.html.twig');
   }
+
+  #[Route("/precieuse", name: "precieusepage")]
+  public function precieuse(): string
+  {
+    return $this->twig->render('precieuse.html.twig');
+  }
+
+  #[Route("/impertinentes", name: "impertinentes")]
+  public function impertinentes(): string
+  {
+    return $this->twig->render('impertinentes.html.twig');
+  }
+
+  #[Route("/unique", name: "unique")]
+  public function unique(): string
+  {
+    return $this->twig->render('unique.html.twig');
+  }
+
+  #[Route("/couleur", name: "couleur")]
+  public function couleur(): string
+  {
+    return $this->twig->render('couleur.html.twig');
+  }
+
+  #[Route("/contact", name: "contact")]
+  public function contact(): string
+  {
+    return $this->twig->render('contact.html.twig');
+  }
+
+  #[Route("/panier", name: "panier")]
+  public function panier(): string
+  {
+    return $this->twig->render('panier.html.twig');
+  }
+
+  #[Route("/blog", name: "blog")]
+  public function blog(): string
+  {
+    return $this->twig->render('blog.html.twig');
+  }
 }
